4c1474b59ebca3ed8614f5b71a7c946ad011d4f7
ElpacinoDZ <ahmedmoundji2014@outlook.fr>
2025-03-09 16:32:18 +0100

    first commit

---
+++ src/Controller/admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin/administrateur', name: 'app.admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/admin.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
---
+++ src/Controller/admin/formations/AdminFormationsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller\admin\formations;
+
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use App\Repository\PlaylistRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminFormationsController extends AbstractController
+{
+    #[Route('/admin/formations', name: 'admin.formations')]
+    public function index(): Response
+    {
+        return $this->render('admin/formations/admin.formations.html.twig', [
+            'controller_name' => 'AdminFormationsController',
+        ]);
+    }
+
+    const PAGE_FORMATIONS = "pages/formations.html.twig";
+    const PAGE_FORMATION = "pages/formation.html.twig";
+
+        /**
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    public function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository= $categorieRepository;
+    }
+      /**
+     * @Route("admin/formations/tri/{champ}/{ordre}/{table}", name="admin.formations.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @param type $table
+     * @return Response
+     */
+    public function sort($champ, $ordre, $table=""): Response{
+        $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(self::PAGE_FORMATIONS, [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+    
+    /**
+     * @Route("admin/formations/recherche/{champ}/{table}", name="admin.formations.findallcontain")
+     * @param type $champ
+     * @param Request $request
+     * @param type $table
+     * @return Response
+     */
+    public function findAllContain($champ, Request $request, $table=""): Response{
+        $valeur = $request->get("recherche");
+        $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(SELF::PAGE_FORMATIONS, [
+            'formations' => $formations,
+            'categories' => $categories,
+            'valeur' => $valeur,
+            'table' => $table
+        ]);
+    }
+    
+    /**
+     * @Route("admin/formations/formation/{id}", name="admin.formations.showone")
+     * @param type $id
+     * @return Response
+     */
+    public function showOne($id): Response{
+        $formation = $this->formationRepository->find($id);
+        return $this->render(SELF::PAGE_FORMATION, [
+            'formation' => $formation
+        ]);
+    }
+}
